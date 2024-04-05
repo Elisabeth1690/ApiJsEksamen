72d36c2ad235bb1bc891140f8c387b20d5d04d2f
Elisabeth1690 <90ebaa@gmail.com>
2024-04-05 10:34:38 +0200

    har endret til store bokstaver på pokenavnen, så nå skal jeg begynne på filtreingen

--- scriptTask1.js
+++ scriptTask1.js
@@ -20,7 +20,7 @@ async function fetchPokemonType() {
   }
 }
 // når jeg skrev inn denne koden første gangen så ble det feil
-//så jeg endret til de orginale navnene, for å så endre tilabke til min
+//så jeg endret til de orginale navnene, for å så endre tilabke til min navn
 // feilen var i den første fetch. men jeg glemte og endre pokeData
 //til pokeDext, på første push.
 function fetchPokemonFullInfo(pokemonFullInfo) {
@@ -35,9 +35,13 @@ function fetchPokemonFullInfo(pokemonFullInfo) {
 function showPokemon(pokeDex) {
   console.log(pokeDex, "pokedex");
   let pokemonCard = document.createElement("div");
-  let pokemonName = pokeDex.name;
+  let pokemonName =
+    pokeDex.name.charAt(0).toUpperCase() + pokeDex.name.slice(1);
+
   let pokemonId = pokeDex.id;
-  let pokemonType = pokeDex.types[0].type.name; // fikk hjelp av chatgpt om hvordan jeg skulle nøste ut bare en type.
+  let pokemonType =
+    pokeDex.types[0].type.name.charAt(0).toUpperCase() +
+    pokeDex.types[0].type.name.slice(1); // fikk hjelp av chatgpt om hvordan jeg skulle nøste ut bare en type.
   console.log("inne i showPokemon", pokemonName, pokemonId, pokemonType);
   pokemonCard.innerHTML = ` 
   <p>#${pokemonId}</p>
@@ -50,64 +54,65 @@ Type: ${pokemonType}</p>
 }
 
 function styleCardColor(pokemonCard, pokemonType) {
-  pokemonCard.style.fontcolor = "black";
   pokemonCard.style.display = "flex";
   pokemonCard.style.flexDirection = "column";
   pokemonCard.style.padding = "5px";
   pokemonCard.style.color = "blue";
-  pokemonCard.style.width = "180px";
+  pokemonCard.style.width = "150px";
   pokemonCard.style.height = "220px";
+  pokemonCard.style.border = "2px solid black";
   pokemonCard.style.borderRadius = "15px";
   pokemonCard.style.margin = "20px";
-  if (pokemonType === "bug") {
+
+  if (pokemonType.toLowerCase() === "bug") {
     pokemonCard.style.backgroundColor = "rgb(168, 243, 114)";
   }
-  if (pokemonType === "dark") {
+  if (pokemonType.toLowerCase() === "dark") {
     pokemonCard.style.backgroundColor = "rgb(124, 125, 123)";
   }
-  if (pokemonType === "electric") {
+  if (pokemonType.toLowerCase() === "electric") {
     pokemonCard.style.backgroundColor = "rgb(255, 247, 88)";
   }
-  if (pokemonType === "fairy") {
+  if (pokemonType.toLowerCase() === "fairy") {
     pokemonCard.style.backgroundColor = "rgb(145, 110, 243)";
   }
-  if (pokemonType === "fighting") {
+  if (pokemonType.toLowerCase() === "fighting") {
     pokemonCard.style.backgroundColor = "rgb(205, 153, 31)";
   }
-  if (pokemonType === "fire") {
+  if (pokemonType.toLowerCase() === "fire") {
     pokemonCard.style.backgroundColor = "rgb(245, 14, 14)";
   }
-  if (pokemonType === "flying") {
+  if (pokemonType.toLowerCase() === "flying") {
     pokemonCard.style.backgroundColor = "rgb(50, 215, 248)";
   }
-  if (pokemonType === "ghost") {
+  if (pokemonType.toLowerCase() === "ghost") {
     pokemonCard.style.backgroundColor = "rgb(176, 210, 216)";
   }
-  if (pokemonType === "grass") {
+  if (pokemonType.toLowerCase() === "grass") {
     pokemonCard.style.backgroundColor = "rgb(116, 198, 57)";
   }
-  if (pokemonType === "ground") {
+  if (pokemonType.toLowerCase() === "ground") {
     pokemonCard.style.backgroundColor = "rgb(189, 144, 85)";
   }
-  if (pokemonType === "ice") {
+  if (pokemonType.toLowerCase() === "ice") {
     pokemonCard.style.backgroundColor = "rgb(190, 223, 229)";
   }
-  if (pokemonType === "normal") {
+  if (pokemonType.toLowerCase() === "normal") {
     pokemonCard.style.backgroundColor = "rgb(216, 168, 101)";
   }
-  if (pokemonType === "poison") {
+  if (pokemonType.toLowerCase() === "poison") {
     pokemonCard.style.backgroundColor = "rgb(100, 66, 221)";
   }
-  if (pokemonType === "pyschic") {
+  if (pokemonType.toLowerCase() === "pyschic") {
     pokemonCard.style.backgroundColor = "rgb(64, 16, 236)";
   }
-  if (pokemonType === "rock") {
+  if (pokemonType.toLowerCase() === "rock") {
     pokemonCard.style.backgroundColor = "rgb(106, 106, 106)";
   }
-  if (pokemonType === "steel") {
+  if (pokemonType.toLowerCase() === "steel") {
     pokemonCard.style.backgroundColor = "rgb(175, 176, 176)";
   }
-  if (pokemonType === "water") {
+  if (pokemonType.toLowerCase() === "water") {
     pokemonCard.style.backgroundColor = "rgb(10, 121, 241)";
   }
 }
