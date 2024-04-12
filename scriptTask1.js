07d85ecf626eb8c994965358c6bd4d27f723ee03
Elisabeth1690 <90ebaa@gmail.com>
2024-04-12 19:55:27 +0200

    tror jeg har fått til slette og redigerings funksjonen på de 50 pokemone og soteringen, men ikke på lagringen

--- scriptTask1.js
+++ scriptTask1.js
@@ -116,6 +116,108 @@ function fetchPokemonFullInfo(pokemonFullInfo) {
     });
 }
 
+/// chatGpt hjalp meg med å finne rikitg måte og flitere på.
+// litt irriterende og spørre hen om dette, siden jeg egentlig kunne det.
+//og har gjort det på staylingen uten hjelp....
+// hadde vist fått hjelp av chatGpt på staylingen også, beklager det!
+
+/////////
+///1.3 lag en pokemon
+const makePokeTypeBtn = document.getElementById("makePokeTypeBtn");
+makePokeTypeBtn.onclick = getCreatePokemontType;
+function getCreatePokemontType() {
+  let createType = document.querySelector("#chosePokemon").value;
+  pokeName(createType);
+}
+function pokeName(createType) {
+  let chosePokeName = prompt("hva vil du at pokemonen din skal hete?");
+  let choseNum = Math.floor(Math.random() * 200) + 50;
+  if (everyPokemonArray.length.name != chosePokeName) {
+    let newPokemonQueen = {
+      name: `${chosePokeName}`,
+      id: `${choseNum}`,
+      types: [{ type: { name: `${createType}` } }],
+    };
+    everyPokemonArray.push(newPokemonQueen);
+    showPokemon();
+  }
+}
+////////////////////////
+////////////////1.4 lagre kort
+
+function savePokeCard(pokeDex) {
+  if (savedPokemonCards.length < 5) {
+    const findCard = savedPokemonCards.some((item) => item.id === pokeDex.id);
+    console.log(pokeDex.id);
+
+    if (!findCard) {
+      savePokemonInLocalStorge(pokeDex);
+      savedPokemonCards.push(pokeDex);
+      fatchSavedPokemonCards();
+    }
+  } else {
+    alert("Du for ikke lagre mer enn 5 favoritter, vær vennlig og slett en");
+  }
+}
+
+function savePokemonInLocalStorge(pokeDex) {
+  if (savedPokemonCards.length < 5) {
+    const savePokemonCardsLocal =
+      JSON.parse(localStorage.getItem("PokemonKort")) || [];
+    savePokemonCardsLocal.push(pokeDex);
+    localStorage.setItem("PokemonKort", JSON.stringify(savePokemonCardsLocal));
+  } else {
+  }
+}
+
+////////
+////1.6 Redigere Pokemon
+function rewritePokemonBtn(pokemonCard, selectPokemonType, index) {
+  let newPokemonName = prompt("Skriv inn ny fornavn");
+  console.log(pokemonCard);
+  if (newPokemonName !== null && newPokemonName.trim() !== "") {
+    pokemonCard.name = newPokemonName;
+    pokemonCard.types[0].type.name = selectPokemonType;
+    console.log(pokemonCard, "nytt kort");
+
+    try {
+      everyPokemonArray[index] = pokemonCard;
+
+      console.log("inne i try nummer2", pokemonCard);
+
+      showPokemon();
+      fatchSavedPokemonCards();
+    } catch (error) {
+      console.error("Feil med endring av navn", error);
+    }
+  }
+}
+
+///////////////
+/////1.5 Slett Pokemon
+function deletePokeCard(index) {
+  const savePokemonCardsLocal =
+    JSON.parse(localStorage.getItem("PokemonKort")) || [];
+  try {
+    everyPokemonArray.splice(index, 1);
+    savedPokemonCards.splice(index, 1);
+    savePokemonCardsLocal.splice(index, 1);
+    localStorage.setItem("PokemonKort", JSON.stringify(savePokemonCardsLocal));
+    console.log(savedPokemonCards);
+  } catch (error) {
+    console.error("klarte ikke og oppdatere arrayet", error);
+  }
+  showPokemon();
+  fatchSavedPokemonCards();
+}
+//////////
+////tøm showPokemonContainer
+function emptyContainer() {
+  showPokemonContainer.innerHTML = "";
+}
+
+//////////////////
+///////vis fram pokemon
 function showPokemon() {
   showPokemonContainer.innerHTML = "";
   everyPokemonArray.forEach((pokeDex, index) => {
@@ -186,62 +288,6 @@ Type: ${pokemonType}</p>
   });
 }
 
-/// chatGpt hjalp meg med å finne rikitg måte og flitere på.
-// litt irriterende og spørre hen om dette, siden jeg egentlig kunne det.
-//og har gjort det på staylingen uten hjelp....
-// hadde vist fått hjelp av chatGpt på staylingen også, beklager det!
-
-/////////
-///1.3 lag en pokemon
-const makePokeTypeBtn = document.getElementById("makePokeTypeBtn");
-makePokeTypeBtn.onclick = getCreatePokemontType;
-function getCreatePokemontType() {
-  let createType = document.querySelector("#chosePokemon").value;
-  pokeName(createType);
-}
-function pokeName(createType) {
-  let chosePokeName = prompt("hva vil du at pokemonen din skal hete?");
-  let choseNum = Math.floor(Math.random() * 200) + 50;
-  if (everyPokemonArray.length.name != chosePokeName) {
-    let newPokemonQueen = {
-      name: `${chosePokeName}`,
-      id: `${choseNum}`,
-      types: [{ type: { name: `${createType}` } }],
-    };
-    everyPokemonArray.push(newPokemonQueen);
-    console.log(everyPokemonArray, newPokemonQueen);
-    fatchSavedPokemonCards();
-    showPokemon();
-  }
-}
-////////////////////////
-////////////////1.4 lagre kort
-
-function savePokeCard(pokeDex) {
-  if (savedPokemonCards.length < 5) {
-    const findCard = savedPokemonCards.some((item) => item.id === pokeDex.id);
-    console.log(pokeDex.id);
-
-    if (!findCard) {
-      savePokemonInLocalStorge(pokeDex);
-      savedPokemonCards.push(pokeDex);
-      everyPokemonArray.push(pokeDex);
-      fatchSavedPokemonCards();
-    }
-  } else {
-    alert("Du for ikke lagre mer enn 5 favoritter, vær vennlig og slett en");
-  }
-}
-
-function savePokemonInLocalStorge(pokeDex) {
-  if (savedPokemonCards.length < 5) {
-    const savePokemonCardsLocal =
-      JSON.parse(localStorage.getItem("PokemonKort")) || [];
-    savePokemonCardsLocal.push(pokeDex);
-    localStorage.setItem("PokemonKort", JSON.stringify(savePokemonCardsLocal));
-  } else {
-  }
-}
 //////////////
 ///////Hente lagrde Kort
 
@@ -251,6 +297,7 @@ function fatchSavedPokemonCards() {
     JSON.parse(localStorage.getItem("PokemonKort")) || [];
   savedCardsContainer.innerHTML = "";
   savePokemonCardsLocal.forEach((pokemon, index) => {
+    console.log(savePokemonCardsLocal, "hva skjer her", pokemon);
     let pokemonSavedCard = document.createElement("div");
     let pokemonName =
       pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
@@ -298,7 +345,7 @@ function fatchSavedPokemonCards() {
     rewritePokeBtn.addEventListener("click", () => {
       const selectPokemonType =
         pokemonSavedCard.querySelector(".chosePokemonTyp").value;
-      rewritePokemonBtn(pokemonSavedCard, selectPokemonType, index);
+      rewritePokemonBtn(pokemon, selectPokemonType, index);
     });
 
     const deleteBtn = document.querySelector(
@@ -311,127 +358,6 @@ function fatchSavedPokemonCards() {
   });
 }
 
-////////
-////1.6 Redigere Pokemon
-function rewritePokemonBtn(pokemonCard, selectPokemonType, index) {
-  let newPokemonName = prompt("Skriv inn ny fornavn");
-
-  if (newPokemonName !== null && newPokemonName.trim() !== "") {
-    pokemonCard.name = newPokemonName;
-    pokemonCard.types[0].type.name = selectPokemonType;
-    try {
-      console.log("inne i try nummer1", pokemonCard);
-
-      savedPokemonCards[index] = pokemonCard;
-      everyPokemonArray[index] = pokemonCard;
-      const savePokemonCardsLocal =
-        JSON.parse(localStorage.getItem("PokemonKort")) || [];
-      savePokemonCardsLocal[index] = pokemonCard;
-      console.log("inne i try nummer2", pokemonCard);
-      localStorage.setItem(
-        "PokemonKort",
-        JSON.stringify(savePokemonCardsLocal)
-      );
-      showPokemon();
-      fatchSavedPokemonCards();
-    } catch (error) {
-      console.error("Feil med endring av navn", error);
-    }
-  }
-}
-
-///////////////
-/////1.5 Slett Pokemon
-function deletePokeCard(index) {
-  console.log("inne i slett");
-  const savePokemonCardsLocal =
-    JSON.parse(localStorage.getItem("PokemonKort")) || [];
-
-  try {
-    savePokemonCardsLocal.splice(index, 1);
-    savedPokemonCards.splice(index, 1);
-    everyPokemonArray.splice(index, 1);
-    localStorage.setItem("PokemonKort", JSON.stringify(savePokemonCardsLocal));
-  } catch (error) {
-    console.error("klarte ikke og oppdatere arrayet", error);
-  }
-  showPokemon();
-  fatchSavedPokemonCards();
-}
-//////////
-////tøm showPokemonContainer
-function emptyContainer() {
-  showPokemonContainer.innerHTML = "";
-}
-
-//////////////////////////
-///////////// styling av kort
-function styleCardColor(pokemonCard, pokemonType) {
-  pokemonCard.style.display = "flex";
-  pokemonCard.style.flexDirection = "column";
-  pokemonCard.style.padding = "5px";
-  pokemonCard.style.color = "blue";
-  pokemonCard.style.width = "150px";
-  pokemonCard.style.height = "450px";
-  pokemonCard.style.border = "2px solid black";
-  pokemonCard.style.borderRadius = "15px";
-  pokemonCard.style.margin = "20px";
-
-  if (pokemonType.toLowerCase() === "bug") {
-    pokemonCard.style.backgroundColor = "rgb(168, 243, 114)";
-  }
-  if (pokemonType.toLowerCase() === "dark") {
-    pokemonCard.style.backgroundColor = "rgb(124, 125, 123)";
-  }
-  if (pokemonType.toLowerCase() === "dragon") {
-    pokemonCard.style.backgroundColor = "rgb(248, 74, 178)";
-  }
-  if (pokemonType.toLowerCase() === "electric") {
-    pokemonCard.style.backgroundColor = "rgb(255, 247, 88)";
-  }
-  if (pokemonType.toLowerCase() === "fairy") {
-    pokemonCard.style.backgroundColor = "rgb(145, 110, 243)";
-  }
-  if (pokemonType.toLowerCase() === "fighting") {
-    pokemonCard.style.backgroundColor = "rgb(205, 153, 31)";
-  }
-  if (pokemonType.toLowerCase() === "fire") {
-    pokemonCard.style.backgroundColor = "rgb(245, 14, 14)";
-  }
-  if (pokemonType.toLowerCase() === "flying") {
-    pokemonCard.style.backgroundColor = "rgb(50, 215, 248)";
-  }
-  if (pokemonType.toLowerCase() === "ghost") {
-    pokemonCard.style.backgroundColor = "rgb(176, 210, 216)";
-  }
-  if (pokemonType.toLowerCase() === "grass") {
-    pokemonCard.style.backgroundColor = "rgb(116, 198, 57)";
-  }
-  if (pokemonType.toLowerCase() === "ground") {
-    pokemonCard.style.backgroundColor = "rgb(189, 144, 85)";
-  }
-  if (pokemonType.toLowerCase() === "ice") {
-    pokemonCard.style.backgroundColor = "rgb(190, 223, 229)";
-  }
-  if (pokemonType.toLowerCase() === "normal") {
-    pokemonCard.style.backgroundColor = "rgb(216, 168, 101)";
-  }
-  if (pokemonType.toLowerCase() === "poison") {
-    pokemonCard.style.backgroundColor = "rgb(100, 66, 221)";
-  }
-  if (pokemonType.toLowerCase() === "psychic") {
-    pokemonCard.style.backgroundColor = "rgb(64, 16, 236)";
-  }
-  if (pokemonType.toLowerCase() === "rock") {
-    pokemonCard.style.backgroundColor = "rgb(106, 106, 106)";
-  }
-  if (pokemonType.toLowerCase() === "steel") {
-    pokemonCard.style.backgroundColor = "rgb(175, 176, 176)";
-  }
-  if (pokemonType.toLowerCase() === "water") {
-    pokemonCard.style.backgroundColor = "rgb(10, 121, 241)";
-  }
-}
 //////////////////////////////////////////////////
 ///////////// sotere pokemone
 function sootPokomone(type) {
@@ -520,4 +446,73 @@ Type: ${pokemonType}</p>
       deletePokeCard(index);
     });
   });
+}
+
+//////////////////////////
+///////////// styling av kort
+function styleCardColor(pokemonCard, pokemonType) {
+  pokemonCard.style.display = "flex";
+  pokemonCard.style.flexDirection = "column";
+  pokemonCard.style.padding = "5px";
+  pokemonCard.style.color = "blue";
+  pokemonCard.style.width = "150px";
+  pokemonCard.style.height = "450px";
+  pokemonCard.style.border = "2px solid black";
+  pokemonCard.style.borderRadius = "15px";
+  pokemonCard.style.margin = "20px";
+
+  if (pokemonType.toLowerCase() === "bug") {
+    pokemonCard.style.backgroundColor = "rgb(168, 243, 114)";
+  }
+  if (pokemonType.toLowerCase() === "dark") {
+    pokemonCard.style.backgroundColor = "rgb(124, 125, 123)";
+  }
+  if (pokemonType.toLowerCase() === "dragon") {
+    pokemonCard.style.backgroundColor = "rgb(248, 74, 178)";
+  }
+  if (pokemonType.toLowerCase() === "electric") {
+    pokemonCard.style.backgroundColor = "rgb(255, 247, 88)";
+  }
+  if (pokemonType.toLowerCase() === "fairy") {
+    pokemonCard.style.backgroundColor = "rgb(145, 110, 243)";
+  }
+  if (pokemonType.toLowerCase() === "fighting") {
+    pokemonCard.style.backgroundColor = "rgb(205, 153, 31)";
+  }
+  if (pokemonType.toLowerCase() === "fire") {
+    pokemonCard.style.backgroundColor = "rgb(245, 14, 14)";
+  }
+  if (pokemonType.toLowerCase() === "flying") {
+    pokemonCard.style.backgroundColor = "rgb(50, 215, 248)";
+  }
+  if (pokemonType.toLowerCase() === "ghost") {
+    pokemonCard.style.backgroundColor = "rgb(176, 210, 216)";
+  }
+  if (pokemonType.toLowerCase() === "grass") {
+    pokemonCard.style.backgroundColor = "rgb(116, 198, 57)";
+  }
+  if (pokemonType.toLowerCase() === "ground") {
+    pokemonCard.style.backgroundColor = "rgb(189, 144, 85)";
+  }
+  if (pokemonType.toLowerCase() === "ice") {
+    pokemonCard.style.backgroundColor = "rgb(190, 223, 229)";
+  }
+  if (pokemonType.toLowerCase() === "normal") {
+    pokemonCard.style.backgroundColor = "rgb(216, 168, 101)";
+  }
+  if (pokemonType.toLowerCase() === "poison") {
+    pokemonCard.style.backgroundColor = "rgb(100, 66, 221)";
+  }
+  if (pokemonType.toLowerCase() === "psychic") {
+    pokemonCard.style.backgroundColor = "rgb(64, 16, 236)";
+  }
+  if (pokemonType.toLowerCase() === "rock") {
+    pokemonCard.style.backgroundColor = "rgb(106, 106, 106)";
+  }
+  if (pokemonType.toLowerCase() === "steel") {
+    pokemonCard.style.backgroundColor = "rgb(175, 176, 176)";
+  }
+  if (pokemonType.toLowerCase() === "water") {
+    pokemonCard.style.backgroundColor = "rgb(10, 121, 241)";
+  }
 }
